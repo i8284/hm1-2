00317ae08bf6e59a83e2b1045d1c0a66c8a2d109
i8284 <109680222+i8284@users.noreply.github.com>
2025-03-19 21:12:08 +0300

    Add files via upload

---
+++ hm/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int income = 44000;
+        int spending = 30000;
+        String x = "Итого: ";
+        System.out.println(x);
+        System.out.println(income - spending);
+    }
+}
